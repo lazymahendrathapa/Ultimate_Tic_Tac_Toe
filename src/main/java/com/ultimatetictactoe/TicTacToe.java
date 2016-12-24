b399f1e0883c8284fe2f067094b51a6679771e4d
Mahendra Thapa <Mahendra@fusemachines.com>
2016-12-24 19:40:07 +0545

    Configure TicTacToe as a JavaFx Application

---
+++ src/main/java/com/ultimatetictactoe/TicTacToe.java
@@ -0,0 +1,14 @@
+package com.ultimatetictactoe;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TicTacToe extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
